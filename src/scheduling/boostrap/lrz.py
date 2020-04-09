018c97aecf2caf731a85b616d16003335613e505
Onno Valkering <onnovalkering@users.noreply.github.com>
2020-04-09 10:33:43 +0200

    Update LRZ charliecloud module load

--- src/scheduling/boostrap/lrz.py
+++ src/scheduling/boostrap/lrz.py
@@ -7,12 +7,12 @@ def get_lrz_bootstrap_script(directory, image, arguments):
 
     script += '#!/bin/bash' + '\n'
     script += 'module load slurm_setup/defaut' + '\n'
-    script += 'module load charliecloud/0.10' + '\n'
+    script += 'module load charliecloud' + '\n'
     script += 'curl -L https://git.io/JvvIy -o chaplin && chmod +x chaplin' + '\n'
     script += f'./chaplin -n -d {image}' + '\n'
 
     image = basename(image)
 
-    script += f'ch-run -w {image} -b {directory}:/local -- bash /var/local/entrypoint.sh {arguments}' + '\n'
+    script += f'ch-run -c "/home/$USER" -w {image} -b {directory}:/local -- bash /var/local/entrypoint.sh {arguments}' + '\n'
 
     return script
